0e1293d628e35b9b6b65cbbc8ffd86707d1cf7ec
Evans Addo-Sampong <addosampong@gmail.com>
2023-02-23 22:56:43 +0000

    made modificaitons to DM and Views

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
 --
--- Host: 127.0.0.1    Database: LittleLemonDB
+-- Host: 127.0.0.1    Database: littlelemondb
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
@@ -14,6 +14,360 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Bookings`
+--
+
+DROP TABLE IF EXISTS `Bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Bookings` (
+  `BookingID` int NOT NULL AUTO_INCREMENT,
+  `TableNo` int NOT NULL,
+  `CustomerID` int NOT NULL,
+  `BookingSlot` time NOT NULL,
+  `EmployeeID` int DEFAULT NULL,
+  PRIMARY KEY (`BookingID`),
+  KEY `customer_id_fk_idx` (`CustomerID`),
+  KEY `employee_id_fk_idx` (`EmployeeID`),
+  CONSTRAINT `customer_id_fk` FOREIGN KEY (`CustomerID`) REFERENCES `Customers` (`CustomerID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `employee_id_fk` FOREIGN KEY (`EmployeeID`) REFERENCES `Employees` (`EmployeeID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Bookings`
+--
+
+LOCK TABLES `Bookings` WRITE;
+/*!40000 ALTER TABLE `Bookings` DISABLE KEYS */;
+INSERT INTO `Bookings` VALUES (1,12,1,'19:00:00',1),(2,12,2,'19:00:00',1),(3,19,3,'15:00:00',3),(4,15,4,'17:30:00',4),(5,5,5,'18:30:00',2),(6,8,6,'20:00:00',5);
+/*!40000 ALTER TABLE `Bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `customer_order_view`
+--
+
+DROP TABLE IF EXISTS `customer_order_view`;
+/*!50001 DROP VIEW IF EXISTS `customer_order_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `customer_order_view` AS SELECT 
+ 1 AS `Customer ID`,
+ 1 AS `Customer`,
+ 1 AS `Order ID`,
+ 1 AS `Cost`,
+ 1 AS `Menu Name`,
+ 1 AS `Course Name`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `Customers`
+--
+
+DROP TABLE IF EXISTS `Customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Customers` (
+  `CustomerID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `ContactNumber` varchar(45) NOT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`CustomerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customers`
+--
+
+LOCK TABLES `Customers` WRITE;
+/*!40000 ALTER TABLE `Customers` DISABLE KEYS */;
+INSERT INTO `Customers` VALUES (1,'Anna','Iversen','555-666-777','anna@email.com'),(2,'Joakim','Iversen','253-643-542','joakim@email.com'),(3,'Vanessa','McCarthy','123-654-745','vanessa@email.com'),(4,'Marcos','Romero','326-462-124','marcos@email.com'),(5,'Hiroki','Yamane','635-789-541','hiroki@email.com'),(6,'Diana','Pinto','364-654-547','diana@email.com');
+/*!40000 ALTER TABLE `Customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Employees`
+--
+
+DROP TABLE IF EXISTS `Employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Employees` (
+  `EmployeeID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Role` varchar(50) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `ContactNumber` int NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `AnnualSalary` varchar(50) NOT NULL,
+  PRIMARY KEY (`EmployeeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Employees`
+--
+
+LOCK TABLES `Employees` WRITE;
+/*!40000 ALTER TABLE `Employees` DISABLE KEYS */;
+INSERT INTO `Employees` VALUES (1,'Mario Gollini','Manager','724, Parsley Lane, Old Town, Chicago, IL',351258074,'Mario.g@littlelemon.com','$70,000'),(2,'Adrian Gollini','Assistant Manager','334, Dill Square, Lincoln Park, Chicago, IL',351474048,'Adrian.g@littlelemon.com','$65,000'),(3,'Giorgos Dioudis','Head Chef','879 Sage Street, West Loop, Chicago, IL',351970582,'Giorgos.d@littlelemon.com','$50,000'),(4,'Fatma Kaya','Assistant Chef','132  Bay Lane, Chicago, IL',351963569,'Fatma.k@littlelemon.com','$45,000'),(5,'Elena Salvai','Head Waiter','989 Thyme Square, EdgeWater, Chicago, IL',351074198,'Elena.s@littlelemon.com','$40,000'),(6,'John Millar','Receptionist','245 Dill Square, Lincoln Park, Chicago, IL',351584508,'John.m@littlelemon.com','$35,000');
+/*!40000 ALTER TABLE `Employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `menu_quantity_view`
+--
+
+DROP TABLE IF EXISTS `menu_quantity_view`;
+/*!50001 DROP VIEW IF EXISTS `menu_quantity_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `menu_quantity_view` AS SELECT 
+ 1 AS `Menu Name`,
+ 1 AS `Course Name`,
+ 1 AS `Quantity`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `MenuItems`
+--
+
+DROP TABLE IF EXISTS `MenuItems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MenuItems` (
+  `ItemID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Type` varchar(100) NOT NULL,
+  `Price` int NOT NULL,
+  PRIMARY KEY (`ItemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MenuItems`
+--
+
+LOCK TABLES `MenuItems` WRITE;
+/*!40000 ALTER TABLE `MenuItems` DISABLE KEYS */;
+INSERT INTO `MenuItems` VALUES (1,'Olives','Starters',5),(2,'Flatbread','Starters',5),(3,'Minestrone','Starters',8),(4,'Tomato bread','Starters',8),(5,'Falafel','Starters',7),(6,'Hummus','Starters',5),(7,'Greek salad','Main Courses',15),(8,'Bean soup','Main Courses',12),(9,'Pizza','Main Courses',15),(10,'Greek yoghurt','Desserts',7),(11,'Ice cream','Desserts',6),(12,'Cheesecake','Desserts',4),(13,'Athens White wine','Drinks',25),(14,'Corfu Red Wine','Drinks',30),(15,'Turkish Coffee','Drinks',10),(16,'Turkish Coffee','Drinks',10),(17,'Kabasa','Main Courses',17);
+/*!40000 ALTER TABLE `MenuItems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Menus`
+--
+
+DROP TABLE IF EXISTS `Menus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Menus` (
+  `MenuID` int NOT NULL,
+  `ItemID` int NOT NULL,
+  `Cuisine` varchar(100) NOT NULL,
+  PRIMARY KEY (`MenuID`),
+  KEY `item_id_fk_idx` (`ItemID`),
+  CONSTRAINT `item_id_fk` FOREIGN KEY (`ItemID`) REFERENCES `MenuItems` (`ItemID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Menus`
+--
+
+LOCK TABLES `Menus` WRITE;
+/*!40000 ALTER TABLE `Menus` DISABLE KEYS */;
+INSERT INTO `Menus` VALUES (1,1,'Greek'),(2,7,'Greek'),(3,10,'Greek'),(4,13,'Greek'),(5,3,'Italian'),(6,9,'Italian'),(7,12,'Italian'),(8,15,'Italian'),(9,5,'Turkish'),(10,17,'Turkish'),(11,11,'Turkish'),(12,16,'Turkish');
+/*!40000 ALTER TABLE `Menus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Orders` (
+  `OrderID` int NOT NULL,
+  `TableNo` int NOT NULL,
+  `MenuID` int NOT NULL,
+  `BookingID` int NOT NULL,
+  `BillAmount` int NOT NULL,
+  `Quantity` int NOT NULL,
+  PRIMARY KEY (`OrderID`,`TableNo`),
+  KEY `menu_id_fk_idx` (`MenuID`),
+  KEY `booking_id_fk_idx` (`BookingID`),
+  CONSTRAINT `booking_id_fk` FOREIGN KEY (`BookingID`) REFERENCES `Bookings` (`BookingID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `menu_id_fk` FOREIGN KEY (`MenuID`) REFERENCES `Menus` (`MenuID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders`
+--
+
+LOCK TABLES `Orders` WRITE;
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+INSERT INTO `Orders` VALUES (1,12,1,1,86,2),(2,19,2,2,37,1),(3,15,2,3,37,1),(4,5,3,4,40,1),(5,8,1,5,43,1),(6,12,1,1,86,4);
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `orders_view`
+--
+
+DROP TABLE IF EXISTS `orders_view`;
+/*!50001 DROP VIEW IF EXISTS `orders_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `orders_view` AS SELECT 
+ 1 AS `Order ID`,
+ 1 AS `Quantity`,
+ 1 AS `Cost`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping events for database 'littlelemondb'
+--
+
+--
+-- Dumping routines for database 'littlelemondb'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `BasicSalesReport` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dataceum`@`localhost` PROCEDURE `BasicSalesReport`()
+BEGIN
+SELECT SUM(BillAmount) AS "Total Sales",
+ROUND(AVG(BillAmount), 2) AS "Average Bill Paid",
+MAX(BillAmount) AS "Maximum Bill Paid",
+MIN(BillAmount) AS "Minimum Bill Paid"
+FROM Orders;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GuestStatus` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dataceum`@`localhost` PROCEDURE `GuestStatus`()
+BEGIN
+SELECT CONCAT(c.FirstName, " ", c.LastName) AS "Guest",
+        (CASE
+        WHEN e.Role IN ("Manager",  "Assistant Manager") THEN "Ready to pay"
+        WHEN e.Role = "Head Chef" THEN "Ready to serve"
+        WHEN e.Role = "Assistant Chef" THEN "Preparing to order"
+        WHEN e.Role = "Head Waiter" THEN "Order served"
+        ELSE "Waiting to be seated"
+        END) AS "Order Status"
+FROM Bookings b
+LEFT JOIN Employees e ON b.EmployeeID = e.EmployeeID
+INNER JOIN Customers c ON b.CustomerID = c.CustomerID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `PeakHours` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dataceum`@`localhost` PROCEDURE `PeakHours`()
+BEGIN
+SELECT HOUR(BookingSlot) AS "Hour",
+    COUNT(BookingSlot) AS "No. of Bookings"
+FROM Bookings
+GROUP BY 1
+ORDER BY 2 DESC;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `customer_order_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `customer_order_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `customer_order_view` AS select `c`.`CustomerID` AS `Customer ID`,concat(`c`.`FirstName`,' ',`c`.`LastName`) AS `Customer`,`o`.`OrderID` AS `Order ID`,round((`o`.`BillAmount` * `o`.`Quantity`),2) AS `Cost`,`m`.`Cuisine` AS `Menu Name`,`mi`.`Name` AS `Course Name` from ((((`customers` `c` join `bookings` `b` on((`c`.`CustomerID` = `b`.`CustomerID`))) join `orders` `o` on((`b`.`BookingID` = `o`.`BookingID`))) join `menus` `m` on((`o`.`MenuID` = `m`.`MenuID`))) join `menuitems` `mi` on((`m`.`ItemID` = `mi`.`ItemID`))) where ((`o`.`BillAmount` * `o`.`Quantity`) > 150) order by (`o`.`BillAmount` * `o`.`Quantity`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `menu_quantity_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `menu_quantity_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `menu_quantity_view` AS select `m`.`Cuisine` AS `Menu Name`,`mi`.`Name` AS `Course Name`,`o`.`Quantity` AS `Quantity` from ((`menuitems` `mi` join `menus` `m` on((`mi`.`ItemID` = `m`.`ItemID`))) join `orders` `o` on((`m`.`MenuID` = `o`.`MenuID`))) where `o`.`Quantity` in (select `orders`.`Quantity` from `orders` where (`orders`.`Quantity` > 2)) order by `o`.`Quantity` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `orders_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `orders_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `orders_view` AS select `orders`.`OrderID` AS `Order ID`,`orders`.`Quantity` AS `Quantity`,round((`orders`.`BillAmount` * `orders`.`Quantity`),2) AS `Cost` from `orders` where (`orders`.`Quantity` > 2) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +378,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-23  8:35:46
+-- Dump completed on 2023-02-23 22:41:12
